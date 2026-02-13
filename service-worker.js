203a56af8ce0cc7527dd46c7ae3006418ce00396
VenomSnakeTwitch <Venomsnaketwitch@gmail.com>
2026-02-13 05:58:57 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,20 @@
-<script>
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js");
-}
-</script>
+// Installation: Dateien cachen
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("app-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html"
+      ]);
+    })
+  );
+});
+
+// Abruf: zuerst aus Cache, dann aus Netzwerk
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
